c8f4ffb7f15b02be8b5e0cf15d1a6e541f921729
rasedul99 <rasedul2199@gmail.com>
2022-02-26 03:20:03 +0600

    bootstrap card add

--- js/index.js
+++ js/index.js
@@ -5,7 +5,16 @@ const loadDog = () => {
 };
 
 const displayDog = (dogList) => {
-  const main = document.getElementById("main");
-  console.log(main);
   console.log(dogList);
+  const wrapper = document.getElementById("wrapper");
+
+  for (const dog of dogList) {
+    const div = document.createElement("div");
+    div.className = "col-lg-4";
+    div.innerHTML = ` 
+  <h2>${dog.name}</h2>
+  <img src="${dog.image.url}" width='400px' height='250px' alt="...">`;
+
+    wrapper.appendChild(div);
+  }
 };
